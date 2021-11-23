d2d0d15fa008edd9d900f0b113e4f48b7269d83f
Elżbieta Janus-Kaleta <60756867+ElaJK01@users.noreply.github.com>
2021-11-23 12:47:21 +0100

    update display and clear functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 // const actions = require('./actions');
+
+const calculator = {
+  displayValue: "0",
+  firstActionOperand: null,
+  
+}
+
+
+function updateDisplay() {
+  const displayResult = document.querySelector('[data-result]')
+  displayResult.value = calculator.displayValue
+}
+
+updateDisplay()
+
+function clear() {
+  const displayResult = document.querySelector('[data-result]')
+  displayResult.value = calculator.displayValue = ""
+}
+
+clear()
+
